7f7e0eb36ba4bf1a71aa36679169275c083f9aca
mani <mmaanniigghh@gmail.com>
2026-02-19 16:31:57 -0800

    remove from gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html", "./src/input.css", "./src/script.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
